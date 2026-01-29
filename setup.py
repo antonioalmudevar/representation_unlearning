31625e5462cd3d56fa982f0825159d9a1424a19b
Antonio Almudévar <91321976+antonioalmudevar@users.noreply.github.com>
2026-01-29 11:36:26 +0100

    Add files via upload

--- setup.py
+++ setup.py
@@ -3,5 +3,7 @@ from setuptools import setup
 setup(  name='representation_unlearning',
         version='1.0',
         description='Representation Unlearning: Forgetting through Information Compression',
+        author='Antonio Almudevar',
+        author_email='almudevar@unizar.es',
         packages=['src'],
     )
